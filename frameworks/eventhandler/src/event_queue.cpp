7f5a2f5b9220a93b68b49929133d3fdd9b6b5ea6
wanglai.yao <yaowanglai@huawei.com>
2025-01-16 13:54:04 +0000

    update frameworks/eventhandler/src/event_queue.cpp.
    Signed-off-by: wanglai.yao <yaowanglai@huawei.com>

--- frameworks/eventhandler/src/event_queue.cpp
+++ frameworks/eventhandler/src/event_queue.cpp
@@ -332,11 +332,11 @@ void EventQueue::SetVsyncLazyMode(bool isLazy)
 
         bool ret;
         if (isLazy) {
-            ret = ioWaiter_->AddFileDescriptor(it->first, FILE_DESCRIPTOR_INPUT_EVENT, fdInfo->taskName_, fdInfo->listener_,
-                Priority::HIGH);
+            ret = ioWaiter_->AddFileDescriptor(it->first, FILE_DESCRIPTOR_INPUT_EVENT, fdInfo->taskName_,
+                fdInfo->listener_, Priority::HIGH);
         } else {
-            ret = DeamonIoWaiter::GetInstance().AddFileDescriptor(it->first, FILE_DESCRIPTOR_INPUT_EVENT, fdInfo->taskName_,
-                fdInfo->listener_, vsyncPriorityOnDaemon_);
+            ret = DeamonIoWaiter::GetInstance().AddFileDescriptor(it->first, FILE_DESCRIPTOR_INPUT_EVENT,
+                fdInfo->taskName_, fdInfo->listener_, vsyncPriorityOnDaemon_);
         }
         if (!ret) {
             HILOGW("%{public}s, AddFileDescriptor failed! fd = %{public}d, name = %{public}s, ret = %{public}d",
